9453a871e58431129b72d6f00aa8cbe0791266a5
Tolgar Sasmaz <sasmazt90@gmail.com>
2025-12-25 20:51:47 +0100

    Add function to cap image width

---
+++ services/utils.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+
+
+def cap_width(img: np.ndarray, max_w: int) -> np.ndarray:
+    h, w = img.shape[:2]
+    if w <= max_w:
+        return img
+    scale = max_w / w
+    return cv2.resize(img, (max_w, int(h * scale)), interpolation=cv2.INTER_AREA)
