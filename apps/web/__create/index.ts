733812ca3913e6d290c0d66baa136565e4d061a6
sasmazt90 <sasmazt90@gmail.com>
2026-05-16 11:36:37 +0200

    Set auth API base path

--- apps/web/__create/index.ts
+++ apps/web/__create/index.ts
@@ -128,6 +128,7 @@ if (process.env.AUTH_SECRET) {
     initAuthConfig((c) => ({
       secret: c.env.AUTH_SECRET,
       trustHost: true,
+      basePath: '/api/auth',
       pages: {
         signIn: '/account/signin',
         signOut: '/account/logout',
